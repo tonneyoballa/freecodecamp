bfa00302aac87d1161e75eae670b0bb5d18eb712
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-19 10:11:06 -0600

    Update script.js

--- day 1/script.js
+++ day 1/script.js
@@ -5,3 +5,40 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore(){
+    console.log("Going to store."); 
+}
+
+function goCave(){
+    console.log("Going to cave."); 
+}
+
+function fightDragon(){
+    console.log("Fighting dragon."); 
+}
+
+//initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+    const button1 = document.querySelector("text");
+    button1.innerText = "Buy 10 health (10 gold)"; 
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+}
+
