c40efc3538f012b17b1b6823b0eea2d2cb0aa695
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-20 06:40:27 -0600

    Update script.js

--- day 1/script.js
+++ day 1/script.js
@@ -6,7 +6,7 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-const button1 = document.querySelector("#button1");
+const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
@@ -17,31 +17,34 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-function goStore(){
-    console.log("Going to store."); 
-}
-
-function goCave(){
-    console.log("Going to cave."); 
-}
-
-function fightDragon(){
-    console.log("Fighting dragon."); 
-}
-
-//initialize buttons
-
+// initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-    const button1 = document.querySelector("text");
-    button1.innerText = "Buy 10 health (10 gold)"; 
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
 
+function goCave() {
+  console.log("Going to cave.");
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+function buyWeapon() {
+
+}function goTown() {
+
+}
