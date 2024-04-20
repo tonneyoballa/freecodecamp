edc8985bd158d378ab48b4c9b9b2c619722f4186
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-20 13:15:38 -0600

    Update script.js

--- day 1/script.js
+++ day 1/script.js
@@ -16,6 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
 const locations = [
   {
     name: "town square",
@@ -68,17 +74,21 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-if (gold >= 10) {
-  function buyHealth() {
-  gold -= 10;
-  health += 10;
-  goldText.innerText = gold;
-  healthText.innerText = health;
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
   }
 }
 
 function buyWeapon() {
+  if (gold >= 30) {
 
+  }
 }
 
 function fightSlime() {
