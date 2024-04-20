6b83f0ba189a4092623607aa5ebaf886b623566d
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-20 14:21:51 -0600

    Update script.js

--- day 1/script.js
+++ day 1/script.js
@@ -106,7 +106,13 @@ function buyWeapon() {
 }
 
 function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
 
+  }
 }
 
 function fightSlime() {
