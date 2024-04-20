abcd8e97d0c89baebc9fbefde46a7b2c53db614c
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-20 13:56:13 -0600

    Update script.js

--- day 1/script.js
+++ day 1/script.js
@@ -86,9 +86,27 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
+}
+
+function sellWeapon() {
+
 }
 
 function fightSlime() {
