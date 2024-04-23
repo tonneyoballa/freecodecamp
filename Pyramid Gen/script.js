4e7f4e34d4d792225025cd018d879adf2fa1ca33
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-23 16:45:08 -0600

    Update script.js

--- Pyramid Gen/script.js
+++ Pyramid Gen/script.js
@@ -3,5 +3,13 @@ const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
-console.log(i)
+  rows.push(i);
 }
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
