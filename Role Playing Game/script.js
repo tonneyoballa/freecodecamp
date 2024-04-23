5cc8b0b82c9e9aea0b1a7847afc7b0209aa989c6
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-23 02:59:21 -0600

    Update script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -182,7 +182,12 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " You miss."
+  }
+  
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
