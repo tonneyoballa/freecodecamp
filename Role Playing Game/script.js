fa1ca7340bb77cc74c6f13cbde83e190365fae01
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-23 12:36:47 -0600

    Update script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -196,6 +196,12 @@ function attack() {
   }
 }
 
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return (hit)
+}
+
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
