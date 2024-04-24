c7857fde107fdac32319fb703a91c1983e2ecbd6
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-24 09:00:58 -0600

    Update script.js

--- Pyramid Gen/script.js
+++ Pyramid Gen/script.js
@@ -1,18 +1,26 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
 while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
+}
+
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
 }
 
 let result = ""
