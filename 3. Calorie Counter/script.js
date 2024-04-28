902ed3e4fcbf0005ea9ce13a594095268fb73bae
Tonney Oballa <tonneyoballah@gmail.com>
2024-04-28 12:36:42 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -7,7 +7,7 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  console.log("original string: ", str);
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
+
