d5d3b4d06642dc063670c87ef945e49a73705f17
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-01 08:08:59 -0600

    Update script.js

--- 5.Date Formatter/script.js
+++ 5.Date Formatter/script.js
@@ -11,8 +11,11 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
+console.log(exampleSentence);
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
-
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
   }
 });
