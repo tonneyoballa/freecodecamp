e0505e2450778b16f5211e9ff26814390e7e0e0a
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 05:28:47 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -23,5 +23,5 @@ function addEntry() {
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`
+  <input type="number" min = "0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`
 }
