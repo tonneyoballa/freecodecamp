d3be9aefabbf0dd666ae588c564654665a7ae6d8
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 10:10:53 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -55,7 +55,8 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit"
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = <span class="surplusOrDeficit.toLowerCase()"> </span>;
 }
 
 function getCaloriesFromInputs(list) {
