e279f344e53222502f6433f5894d0169e6ca07ba
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 06:50:17 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -37,6 +37,11 @@ function calculateCalories(e) {
   isError = false;
 
   const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
 
 }
 
