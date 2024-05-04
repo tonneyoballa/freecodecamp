9cb25a1f00942e838c700aabaa2dfd1f9315e5c8
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 06:32:48 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -37,6 +37,14 @@ function getCaloriesFromInputs(list) {
 
   for (const item of list) {
     const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
 }
 
