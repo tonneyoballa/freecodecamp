ffdcf93958931d10f541c5bed74febcce4579d1e
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 05:46:33 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -29,7 +29,15 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
-  targetInputContainer.insertAdjacentHTML();
+  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
