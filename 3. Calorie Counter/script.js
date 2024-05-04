0dfe595a1ab9e1c45394229c0e8440e1d51cd7ba
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-04 04:37:24 -0600

    Update script.js

--- 3. Calorie Counter/script.js
+++ 3. Calorie Counter/script.js
@@ -20,7 +20,6 @@ function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
   const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  
-  `;
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
 }
