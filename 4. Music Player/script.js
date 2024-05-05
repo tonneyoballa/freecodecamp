aaef3b12201b5832da6cf12b6ee787fc3204273c
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-05 13:23:54 -0600

    Update script.js

--- 4. Music Player/script.js
+++ 4. Music Player/script.js
@@ -1,6 +1,3 @@
-document.getElementById('#playlist-songs');
-document.getElementById("#play");
-document.getElementById("#pause");
-const playlistSongs = document.getElementById('#playlist-songs');
-const playButton = document.getElementById("#play");
-const pauseButton = document.getElementById("#pause");
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
