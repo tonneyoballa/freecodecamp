db32dcb01011caff05e33928e5bc7e2228c66f7d
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-05 13:32:15 -0600

    Update script.js

--- 4. Music Player/script.js
+++ 4. Music Player/script.js
@@ -1,7 +1,16 @@
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
-const nextButton = document.getElementById("next")
-const previousButton= document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
-const allSongs = [];
+
+const allSongs = [
+{
+  id: 0,
+  title: "Scratching The Surface",
+  artist: "Quincy Larson",
+  duration: "4:25",
+  src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+}
+];
