ba6e37c96f801c805a45c60091e22f0b0b04f1e4
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-10 05:00:13 -0600

    Update script.js

--- 4. Music Player/script.js
+++ 4. Music Player/script.js
@@ -79,22 +79,9 @@ const allSongs = [
 ];
 
 const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
-
-const printGreeting = () => {
-  console.log('Hello there!'); 
-}
-
-printGreeting();
-
-const printMessage = org => {
-  console.log(`${org} is awesome!`);
-}
-
-printMessage('freeCodeCamp');
-const addTwoNumbers = (num1, num2) => num1 + num2;
-console.log(addTwoNumbers(3,4));
