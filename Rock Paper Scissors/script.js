ff67d92dfbc724492f699bd7fd2fc0785fdef625
Tonney Oballa <tonneyoballah@gmail.com>
2024-05-23 07:44:42 -0600

    Update script.js

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -5,9 +5,16 @@ function getRandomComputerResult() {
   }
   
   function hasPlayerWonTheRound(player, computer) {
-    
+    if (player == "Rock" && computer == "Scissors"){
+        return true;
+    }else if(player == "Scissors" && computer == "Paper"){
+      return true;
+    }else if(player == "Paper" && computer == "Rock"){
+      return true;
+    } else {
+      return false;
+    }
   }
   
   console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
-  console.log(hasPlayerWonTheRound("Scissors", "Rock"));
-  
+  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
