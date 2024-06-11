e81679a1e966fa73efb5d3ef8b5b46d5f67fe4fd
Tonney Oballa <tonneyoballah@gmail.com>
2024-06-11 15:56:13 -0600

    Update script.js

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -21,3 +21,15 @@ function getRandomComputerResult() {
   
   console.log(getRoundResults("Rock"));
   console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+
+
+
+
+
+
+
+
+
+
+
+  
